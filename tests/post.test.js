3f69cad0dcc73aa721d07e255bac61cb238234a4
Bar Mor <barmor123456@gmail.com>
2024-04-16 02:06:52 +0300

    AddNewPost Test

--- tests/post.test.js
+++ tests/post.test.js
@@ -4,13 +4,16 @@ const app = require('../server');
 
 let server;
 
+const newPostMessage = 'Bar Mor King';
+const newPostSender = 'Bar Mor';
+
 beforeAll(done => {
     server = app.listen(3000, done);  
 });
 
 afterAll(async () => {
-    await mongoose.connection.close();  // Close MongoDB connection
-    server.close();  // Then close the server
+    await mongoose.connection.close();  
+    server.close();  
 });
 
 
@@ -20,48 +23,19 @@ describe("posts Tests", () => {
         expect(response.statusCode).toEqual(200);
         expect(response.body).toBeInstanceOf(Array);
     });
-        test("Add new post", async () => {
+    test("Add new post", async () => {
         const response = await supertest(app)
             .post('/post')
             .send({
-                message: "Bar Mor King",
-                sender: "Bar Mor"
+                message: newPostMessage,  
+                sender: newPostSender     
             });
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty('message');
-        expect(response.body).toHaveProperty('sender');
+        expect(response.body.message).toEqual(newPostMessage);
+        expect(response.body.sender).toEqual(newPostSender);
     });
-
-    // test("Get post by ID", async () => {
-    //     const response = await supertest(app).get('/post/5f5a192f0b2e9a1c6c4d4b8a');
-    //     expect(response.statusCode).toEqual(200);
-    //     expect(response.body).toHaveProperty('message');
-    //     expect(response.body).toHaveProperty('sender');
-    // });
-
-    // test("Get post by invalid ID", async () => {
-    //     const response = await supertest(app).get('/post/123');
-    //     expect(response.statusCode).toEqual(400);
-    //     expect(response.body).toHaveProperty('message');
-    // });
-
-    // test("Get post by non-existent ID", async () => {
-    //     const response = await supertest(app).get('/post/5f5a192f0b2e9a1c6c4d4b8b');
-    //     expect(response.statusCode).toEqual(404);
-    //     expect(response.body).toHaveProperty('message');
-    // });
-
-
-
-    // test("Add new post with missing data", async () => {
-    //     const response = await supertest(app)
-    //         .post('/post')
-    //         .send({
-    //             message: "Hello, World!"
-    //         });
-    //     expect(response.statusCode).toEqual(400);
-    //     expect(response.body).toHaveProperty('error');
-    // });
-
     
 });
+
+    
+
