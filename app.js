71456509b7c1908eca5c3e018af99275f9dd4a6b
Bar Mor <barmor123456@gmail.com>
2024-04-15 15:52:39 +0300

    all

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port :', process.env.PORT)
+});
