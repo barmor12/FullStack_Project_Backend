79418def46fd4f50ff364fbbf125b1a41455d150
Bar Mor <barmor123456@gmail.com>
2024-04-15 23:48:42 +0300

    Get Posts

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,23 +1,34 @@
 const Post = require('../models/post_model'); 
 
-const getAllPosts = (req, res, next) => {
-    res.send('get all posts');
+
+const getAllPosts = async(req, res, next) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);  
+    } catch (err) {
+        if (!res.headersSent) {  
+            res.status(400).json({ message: 'Error retrieving posts' });
+        }
+    }
 }
+
+
 
 const addNewPost = async (req, res, next) => {
     console.log(req.body);
 
-    try {
-        const post = new Post({  
-            message: req.body.message,
-            sender: req.body.sender
-        });
+    const post = new Post({  
+        message: req.body.message,
+        sender: req.body.sender
+    });
 
-        const newPost = await post.save();  
+    try {
+        
+        newPost = await post.save();  
         console.log("Post saved in db", newPost);
         res.status(200).send(newPost);
     } catch (err) {
-        console.log("Failed to save post in db", err);
+        console.log("Failed to save post in db");
         res.status(400).send('Error: Failed to add new post');
     }
 }
