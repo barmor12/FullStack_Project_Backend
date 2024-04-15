b1b2d5a3dc6d0ac43b0a5add1b019923252bc989
Bar Mor <barmor123456@gmail.com>
2024-04-15 16:42:43 +0300

    post

--- routes/post_route.js
+++ routes/post_route.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();    
-const post = require('..controllers/post_controller.js')
+const post = require('../controllers/post_controller.js');
 
-router.get('/',post.getAllPosts);
-
-router.post('/', (post.AddNewPost));
+router.get('/', post.getAllPosts);
+router.post('/', post.AddNewPost);
 
 module.exports = router;
