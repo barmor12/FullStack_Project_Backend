5ce02be5af9efe6a9d9467cf3fb80286b854d228
Bar Mor <barmor123456@gmail.com>
2024-04-16 12:53:09 +0300

    public middleware

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true,limit:'1mb' }));
 app.use(bodyParser.json()); 
 
-const postRouter = require('./routes/post_route.js');
-app.use('/post', postRouter);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
@@ -15,7 +13,10 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to mongo DB'));
 
+app.use(express.static('public'));
 
+const postRouter = require('./routes/post_route.js');
+app.use('/post', postRouter);
 
 
 
--- tests/post.test.js
+++ tests/post.test.js
@@ -1,41 +1,48 @@
 const supertest = require('supertest');
 const mongoose = require('mongoose');
-const app = require('../server');  
+const app = require('../server');
+const Post_c = require('../models/post_model');
 
 let server;
 
 const newPostMessage = 'Bar Mor King';
 const newPostSender = 'Bar Mor';
+let newPostId = '';
 
-beforeAll(done => {
-    server = app.listen(3000, done);  
+beforeAll(async () => {
+    server = app.listen(3000);
+    await Post_c.deleteMany({});
 });
 
 afterAll(async () => {
-    await mongoose.connection.close();  
-    server.close();  
+    await mongoose.connection.close();
+    if (server) server.close();
 });
 
-
 describe("posts Tests", () => {
+
+    test("Add new post", async () => {
+        const response = await supertest(app)
+            .post('/post')
+            .send({
+                message: newPostMessage,
+                sender: newPostSender
+            });
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.message).toEqual(newPostMessage);
+        expect(response.body.sender).toEqual(newPostSender);
+        newPostId = response.body._id;
+    });
     test("Get all posts", async () => {
         const response = await supertest(app).get('/post');
         expect(response.statusCode).toEqual(200);
         expect(response.body).toBeInstanceOf(Array);
     });
-    test("Add new post", async () => {
-        const response = await supertest(app)
-            .post('/post')
-            .send({
-                message: newPostMessage,  
-                sender: newPostSender     
-            });
+    test("Get post by id", async () => {
+        const response = await supertest(app).get('/post/' + newPostId);
         expect(response.statusCode).toEqual(200);
         expect(response.body.message).toEqual(newPostMessage);
         expect(response.body.sender).toEqual(newPostSender);
-    });
-    
+    })
+
 });
-
-    
-
